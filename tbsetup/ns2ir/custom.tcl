9ad3037020f962d593ce539f0627e0e2a9b611af
Yathindra Naik <ydev@flux.utah.edu>
2012-01-09 15:16:58 -0700

    Added a dummy custom agent class definition file

---
+++ tbsetup/ns2ir/custom.tcl
@@ -0,0 +1,72 @@
+# -*- tcl -*-
+#
+# EMULAB-COPYRIGHT
+# Copyright (c) 2000-2006 University of Utah and the Flux Group.
+# All rights reserved.
+#
+
+######################################################################
+# Custom.tcl
+#
+# This defines the local custom agent.
+#
+######################################################################
+
+Class Custom -superclass NSObject
+
+namespace eval GLOBALS {
+    set new_classes(Custom) {}
+}
+
+Program instproc init {s} {
+    global ::GLOBALS::last_class
+
+    $self set sim $s
+    $self set node {}
+    $self set name {}
+
+    # Link simulator to this new object.
+    $s add_program $self
+
+    set ::GLOBALS::last_class $self
+}
+
+Program instproc rename {old new} {
+    global ::GLOBALS::all_programs
+    $self instvar sim
+
+    $sim rename_program $old $new
+}
+
+# updatedb DB
+# This adds rows to the virt_trafgens table corresponding to this agent.
+Program instproc updatedb {DB} {
+    var_import ::GLOBALS::pid
+    var_import ::GLOBALS::eid
+    var_import ::TBCOMPAT::objtypes
+    $self instvar node
+    $self instvar name 
+    $self instvar sim
+
+    if {$node == {}} {
+	perror "\[updatedb] $self has no node."
+	return
+    }
+    set progvnode $node
+
+    #
+    # if the attached node is a simulated one, we attach the
+    # program to the physical node on which the simulation runs
+    #
+    if {$progvnode != "ops"} {
+	if { [$node set simulated] == 1 } {
+	    set progvnode [$node set nsenode]
+	}
+    }
+
+    # Update the DB
+    spitxml_data "virt_custom" [list "vnode" "vname" "name"] [list $progvnode $self $name]
+
+    $sim spitxml_data "virt_agents" [list "vnode" "vname" "objecttype" ] [list $progvnode $self $objtypes(CUSTOM) ]
+}
+
