a5237fbfdeb17c7afbc95edc5c3eed409d2e100b
Mike Hibler <hibler@cs.utah.edu>
2012-01-10 15:07:56 -0700

    Newnode version of the startup script.
    There is no make target to install this, but I wanted to get this script
    "on the record."

---
+++ clientside/tmcc/freebsd/newnode-testbed.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# EMULAB-COPYRIGHT
+# Copyright (c) 2000-2012 University of Utah and the Flux Group.
+# All rights reserved.
+#
+# PROVIDE: testbed
+#
+
+. /etc/emulab/paths.sh
+
+#
+# Boottime initialization. 
+#
+case "$1" in
+start|faststart)
+	echo ""
+	$BINDIR/newclient
+	;;
+stop)
+	# Foreground mode.
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
