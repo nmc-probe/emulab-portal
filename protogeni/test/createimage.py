750be519d0e55e8993182950cc863bb4c9adabf4
Leigh B Stoller <stoller@flux.utah.edu>
2012-06-07 16:03:17 -0600

    Add new API call CreateImage; create/snapshot a geni sliver node.
    This call allows a geni user to create a sliver, customise the node,
    and then take a snapshot (possibly creating a new image descriptor)
    without having to use the Emulab web interface. The API looks like:
    
      int CreateImage(slice_urn, sliver_urn, imagename, credentials[]);
    
    The slice must be unlocked and the sliver in the ready state. Once
    the operation starts, the slice is locked until the backend finishes.
    This is something that I might revisit later, but this was the easiest
    approach that ensures consistency. 
    
    The imagename is looked up in the current project the node is attached
    to. If it does not exists, create it. Then snapshot the node. You can
    call this again of course, to take a new snapshot, without having to
    provide a new imagename.
    
    The image file is written to the images directory of the project,
    which is available to the node via /proj, so the user can get to it
    (since the user might not have a local account).

---
+++ protogeni/test/createimage.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+#
+# GENIPUBLIC-COPYRIGHT
+# Copyright (c) 2008-2012 University of Utah and the Flux Group.
+# All rights reserved.
+# 
+# Permission to use, copy, modify and distribute this software is hereby
+# granted provided that (1) source code retains these copyright, permission,
+# and disclaimer notices, and (2) redistributions including binaries
+# reproduce the notices in supporting documentation.
+#
+# THE UNIVERSITY OF UTAH ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+# CONDITION.  THE UNIVERSITY OF UTAH DISCLAIMS ANY LIABILITY OF ANY KIND
+# FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+#
+
+#
+#
+import sys
+import pwd
+import getopt
+import os
+import time
+import re
+
+ACCEPTSLICENAME=1
+
+debug    = 0
+impotent = 1
+
+execfile( "test-common.py" )
+
+if len(REQARGS) != 2:
+    Usage()
+    sys.exit(1)
+    pass
+
+imagename  = REQARGS[0]
+sliver_urn = REQARGS[1]
+
+#
+# Get a credential for myself, that allows me to do things at the SA.
+#
+mycredential = get_self_credential()
+print "Got my SA credential"
+
+#
+# Lookup slice and get credential.
+#
+myslice = resolve_slice( SLICENAME, mycredential )
+
+print "Asking for slice credential for " + SLICENAME
+slicecredential = get_slice_credential( myslice, mycredential )
+print "Got the slice credential"
+
+#
+# Create the image
+#
+print "Creating the Image ..."
+params = {}
+params["credentials"] = (slicecredential,)
+params["slice_urn"]   = myslice["urn"]
+params["sliver_urn"]  = sliver_urn
+params["imagename"]   = imagename
+rval,response = do_method("cm", "CreateImage", params, version="2.0")
+if rval:
+    Fatal("Could not create image")
+    pass
+output = response["value"]
+print str(output)
