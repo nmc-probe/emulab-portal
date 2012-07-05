0727964d53443d558888557fce59e8a7a62e4035
Weibin Sun <wbsun@flux.utah.edu>
2012-07-05 17:35:13 -0600

    Arista module code skeleton added. Some getters can already work, I guess. The switch side deamon also added, no real code yet.

---
+++ tbsetup/snmpit_test/snmpit_arista_switch_deamon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#
+# EMULAB-LGPL
+# Copyright (c) 2012 University of Utah and the Flux Group.
+# All rights reserved.
+#
+
+#
+# Deamon server running on Arista switch side for snmpit_arista.
+#
+
+import PyClient
+import EntityManager
+import XMLRPCServer
+
+# TODO finish this. Now just for place holder.
