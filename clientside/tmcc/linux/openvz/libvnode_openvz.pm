e2a45a1df4fbf3c3bfea60d34077c4df4943692a
Leigh B Stoller <stoller@flux.utah.edu>
2012-09-25 16:13:19 -0600

    Some quickie code to pre-create openvz container images so that they do not have to be created at swapin. This is mostly for the tutorial sessions, where we have dozens of containers launching at the same time.

--- clientside/tmcc/linux/openvz/libvnode_openvz.pm
+++ clientside/tmcc/linux/openvz/libvnode_openvz.pm
@@ -910,47 +910,49 @@ sub vz_vnodeCreate {
 	print STDERR "Using LVM with root size $rootSize MB, ".
 	    "snapshot size $snapSize MB.\n";
 
-	# we must have the lock, so if we need to return right away, unlock
-	if (-e $imagelockpath) {
-	    TBScriptUnlock();
-	}
-	else {
-	    if (1) {
-		#
-		# If there is already a logical device for this image, then
-		# need to GC or rename it (might be in use). Note that a
-		# reload of the partition will cause the lock files to get
-		# deleted, which results in some needless work (recreating
-		# the lvm even if it did not change), but I do not see a
-		# way to stamp the lvm itself so that we can determine its
-		# creation date. Besides, it is an atypical case.
-		#
-		if (system("lvdisplay /dev/openvz/$image >& /dev/null") == 0) {
-		    if (GClvm("$image")) {
-			fatal("Could not GC or rename $image");
-		    }
+	#
+	# Got the lock; if the imagelock file does not exists, we have
+	# to create the base lvm for it. Otherwise, hold the lock until
+	# we check the existing lvm cache.
+	#
+	if (! -e $imagelockpath) {
+	    #
+	    # If there is already a logical device for this image, then
+	    # need to GC or rename it (might be in use). Note that a
+	    # reload of the partition will cause the lock files to get
+	    # deleted, which results in some needless work (recreating
+	    # the lvm even if it did not change), but I do not see a
+	    # way to stamp the lvm itself so that we can determine its
+	    # creation date. Besides, it is an atypical case.
+	    #
+	    if (system("lvdisplay /dev/openvz/$image >& /dev/null") == 0) {
+		if (GClvm("$image")) {
+		    fatal("Could not GC or rename $image");
 		}
-		print "Creating LVM core logical device for image $image\n";
-
-		# ok, create the lvm logical volume for this image.
-		mysystem("lvcreate $LVMDEBUGOPTS ".
-			 "  -L${rootSize}M -n $image openvz");
-		mysystem("mkfs -t ext3 /dev/openvz/$image");
-		mysystem("mkdir -p /mnt/$image");
-		mysystem("mount /dev/openvz/$image /mnt/$image");
-		mysystem("mkdir -p /mnt/$image/root ".
-			 "         /mnt/$image/private");
-		mysystem("tar -xzf $imagepath -C /mnt/$image/private");
-		mysystem("umount /mnt/$image")
-		    if ($DOSNAP);
 	    }
+	    print "Creating LVM core logical device for image $image\n";
+
+	    # ok, create the lvm logical volume for this image.
+	    mysystem("lvcreate $LVMDEBUGOPTS ".
+		     "  -L${rootSize}M -n $image openvz");
+	    mysystem("mkfs -t ext3 /dev/openvz/$image");
+	    mysystem("mkdir -p /mnt/$image");
+	    mysystem("mount /dev/openvz/$image /mnt/$image");
+	    mysystem("mkdir -p /mnt/$image/root ".
+		     "         /mnt/$image/private");
+	    mysystem("tar -xzf $imagepath -C /mnt/$image/private");
+	    mysystem("umount /mnt/$image")
+		if ($DOSNAP);
+
 	    # ok, we're done
 	    mysystem("mkdir -p /var/emulab/run");
 	    mysystem("touch $imagelockpath");
-	    TBScriptUnlock();
 	}
 
 	if ($DOSNAP) {
+	    # Snapshots can happen in parallel.
+	    TBScriptUnlock();
+	    
 	    #
 	    # Now take a snapshot of this image's logical device
 	    #
@@ -963,11 +965,25 @@ sub vz_vnodeCreate {
 			 "  -s -L${snapSize}M -n $vnode_id /dev/openvz/$image");
 	    }
 	}
-	else {
+	elsif (system("lvdisplay /dev/openvz/$vnode_id >& /dev/null")) {
 	    #
-	    # No snapshot, create a new disk for each container. 
-	    # 
-	    if (system("lvdisplay /dev/openvz/$vnode_id >& /dev/null")) {
+	    # Need to create a new disk for the container. But lets see
+	    # if we have a disk cached. We still have the imagelock at
+	    # this point.
+	    #
+	    if (my (@files) = glob("/dev/openvz/_C_${image}_*")) {
+		#
+		# Grab the first file and rename it. It becomes ours.
+		# Then drop the lock.
+		#
+		my $file = $files[0];
+		mysystem("lvrename $file /dev/openvz/$vnode_id");
+		TBScriptUnlock();
+	    }
+	    else {
+		# This can happen in parallel with other VMs.
+		TBScriptUnlock();
+	    	    
 		mysystem("lvcreate $LVMDEBUGOPTS ".
 			 "  -L${rootSize}M -n $vnode_id openvz");
 		mysystem("mkfs -t ext3 /dev/openvz/$vnode_id");
