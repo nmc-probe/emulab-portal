310d7fbccdec22eb7a937181dcc2ced6b2ae3b42
Leigh B Stoller <stoller@flux.utah.edu>
2012-10-02 17:04:08 -0600

    Increase boot timeout.

--- clientside/tmcc/linux/mkvnode.pl
+++ clientside/tmcc/linux/mkvnode.pl
@@ -653,7 +653,7 @@ my $childpid = fork();
 if ($childpid) {
     my $timedout = 0;
     local $SIG{ALRM} = sub { kill("TERM", $childpid); $timedout = 1; };
-    alarm 90;
+    alarm 180;
     waitpid($childpid, 0);
     alarm 0;
 
