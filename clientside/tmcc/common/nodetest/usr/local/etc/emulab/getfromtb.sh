81c203fecacb8ed8e11380035f820e92d549dca6
Dan Reading <dreading@flux.utah.edu>
2013-03-04 12:19:28 -0700

    Monday checkpoint checkin cpucheck - speed working but a different approch maybe be better diskcheck - working but need code for machines that do on support smartctl getfromtb.sh - add more machines redo size checks reported sizes in Mb memcheck - working but a different approch maybe be better niccheck - only counts number nics right now timecheck - works but needs to flag smaller diff value, currently 1 sec update - add more machines

--- clientside/tmcc/common/nodetest/usr/local/etc/emulab/getfromtb.sh
+++ clientside/tmcc/common/nodetest/usr/local/etc/emulab/getfromtb.sh
@@ -18,8 +18,8 @@ case ${d[0]} in
 #	    pc219 ) echo "1990948kB" ;;
 
 	    pc4 | pc7 ) echo "256MiB" ;;
-	    pc126 | pc121 ) echo "512MiB" ;;
-	    pc286 | pc219 ) echo "2048MiB" ;;
+	    pc126 | pc121 | pc137 | pc133) echo "512MiB" ;;
+	    pc207 | pc208| pc286 | pc219 ) echo "2048MiB" ;;
 	    ibapah ) echo "8096MiB" ;;
 	    boss.emulab.net ) echo "4096MiB" ;;
 	    pc472 ) echo "12288MiB" ;;
@@ -32,9 +32,21 @@ case ${d[0]} in
 	;;
     diskinfo )
 #	echo "asked for diskinfo node:${d[0]}:"
+	case ${d[1]} in
+	    pc4 | pc7 ) echo "13GB" ;;
+	    pc126 | pc121 | pc137 | pc136 ) echo "41GB" ;;
+	    pc207 | pc208 | pc286 | pc219 ) echo "146GB 146GB" ;;
+	    ibapah ) echo "160GB 500GB 500GB" ;;
+#	    boss.emulab.net ) echo "" ;;
+	    pc472 | pc406 | pc446 ) echo "500GB 250GB" ;;
+	    * ) 
+		echo "unknown_host"
+	        return 1
+		;;
+	esac
 	;;
     * )
-	echo "unknown request"
+	echo "unknown_request"
 	return 1
 	;;
 esac
@@ -43,3 +55,109 @@ return 0
 
 }
 
+
+# smallest chunk to count with, in MiB
+SmallestCNT=256
+KiB=1024
+KiB256=$(($KiB*${SmallestCNT}))  #smallest chunk to count with
+MiB=1048576
+GiB=1073741824
+
+
+#how big is this
+hbis() {
+    number=$1
+#number="250260kB"
+#number="443124kB"
+#number="4000848kB"
+#number="8110204kB"
+#number="13,676,544,000 bytes"
+#number="8388608"
+#number="146,815,733,760 bytes [146 GB]"
+#number="500,107,862,016 bytes [500 GB]"
+#number="[500 GB]"
+    base=""
+
+    # little input checking - remove punction and spaces
+    z=$(echo ${number} | tr -d [:punct:] | tr -d [:space:])
+    # what units is the number in
+    y=$(echo ${z,,}) #lower case letters
+
+    # check for string 'bytes'
+    if [ ${y%%bytes*} != ${y} ] ; then
+	#remove the string 'ytes' and everthing after
+	y=$(echo ${y/ytes*/})
+    fi
+
+    if [ ${y%%m*} != ${y} ] ; then
+	# its in megs
+	base="m"
+	# strip letters
+	number=${y%%m*}
+	bytes=$((${number}*$MiB))
+    elif [ ${y%%g*} != ${y} ] ; then
+	base="g"
+	number=${y%%g*}
+	bytes=$((${number}*$GiB))
+    elif [ ${y%%k*} != ${y} ] ; then
+	number=${y%%k*}
+	base="k"
+	bytes=$((${number}*$KiB))
+    else
+	base="b"
+	z=${y%%[a-z]*}
+	number=${z//,/}
+	bytes=$number
+    fi
+
+#echo base:$base number:$number bytes=$bytes
+    # return numbers in MiB
+    case $base in
+	g )
+	    c=0
+	    for ((x=$GiB;;x+=$GiB)) ; do
+		((++c))
+		[[ $x -ge $bytes ]] && break
+	    done
+	    echo ${c}GiB
+	    ;;
+	m )
+	    c=0
+	    for ((x=$MiB;;x+=$MiB)) ; do
+		((++c))
+		[[ $x -ge $bytes ]] && break
+	    done
+	    echo ${c}MiB
+	    ;;
+
+	k )
+	    c=0
+	    for ((x=${KiB256};;x+=${KiB256})) ; do
+		((++c))
+		[[ $x -ge $number ]] && break
+	    done
+	    [[ $number -ge $(($MiB*4)) ]] && ((++c))
+	    echo $((${c}*${SmallestCNT}))MiB
+	    ;;
+	
+	b )
+	    if (( ${number} < $KiB )) ; then
+		echo ${number}BiB
+	    elif (( ${number} < $MiB )) ; then
+		echo $(($number/$KiB))KiB
+	    elif (( ${number} < $GiB )) ; then
+		echo $(($number/$MiB))MiB
+	    elif (( ${number} = $GiB )) ; then
+		echo $(($number/$GiB))GiB
+	    else
+	    c=0
+	    for ((x=$GiB;;x+=$GiB)) ; do
+		((++c))
+		[[ $x -ge $bytes ]] && break
+	    done
+	    echo ${c}GiB
+	    fi
+	    
+	    ;;
+    esac
+}
