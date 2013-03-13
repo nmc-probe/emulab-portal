a4eacf3a42152377e4f7db57a72777b080b7f5c4
Leigh B Stoller <stoller@flux.utah.edu>
2013-03-13 07:59:53 -0600

    More plone login changes. Almost there.

--- collab/plone/emulab.pas/emulab/pas/plugins/emulab.py
+++ collab/plone/emulab.pas/emulab/pas/plugins/emulab.py
@@ -1,15 +1,21 @@
 import logging
 import cgi
 import urllib
+import re
+import os
 
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 from AccessControl.SecurityInfo import ClassSecurityInfo
+from AccessControl.SecurityManagement import getSecurityManager
 from Products.PluggableAuthService.utils import classImplements
 from Globals import InitializeClass
 from Products.PluggableAuthService.interfaces.plugins import \
-     IAuthenticationPlugin, IUserEnumerationPlugin, IExtractionPlugin
+     IAuthenticationPlugin, IExtractionPlugin, IRolesPlugin
+from Products.PluggableAuthService.interfaces.plugins import \
+     IUserEnumerationPlugin, IGroupsPlugin
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from zope.component.hooks import getSite
+from Products.CMFCore.utils import getToolByName
 
 logger = logging.getLogger('emulabpas')
 outfile = logging.FileHandler(filename='/tmp/emulabpas.log')
@@ -79,10 +85,10 @@ class EmulabPlugin(BasePlugin):
 
         # If there is a user with this id, we do not authenticate
         # on this path, they have to log in normally.
-        user = self._getPAS().getUserById(user_id)
-        if user is not None:
-            logger.debug("User '%s' exists, not doing anything.", user_id)
-            return {}
+        #user = self._getPAS().getUserById(user_id)
+        #if user is not None:
+        #    logger.debug("User '%s' exists, not doing anything.", user_id)
+        #    return {}
 
         result = {}
         result['user_id'] = user_id
@@ -108,15 +114,57 @@ class EmulabPlugin(BasePlugin):
         user_id = credentials['user_id']
 
         #
-        # Consult external somthing
+        # Sanity check the user_id before we use it to open a file.
         #
-        verified = True
-        
-        if verified:
+        if not re.match("^[-\w]*$", user_id):
+            logger.warn("Illegal characters in user_id: " + user_id)
+            return None
+
+        if True:
+            user = self._getPAS().getUserById(user_id)
+            if user:
+                current_group_ids = user.getGroupIds()
+                logger.debug("Groups for user_id: " + str(current_group_ids))
+                if "emulabusers" not in current_group_ids:
+                    group_tool = getToolByName(self, 'portal_groups')
+                    logger.debug("Adding user id %s to group emulabusers", user_id)
+                    group_tool.addPrincipalToGroup(user_id, "emulabusers")
+                    pass
+                pass
             return (user_id, user_id)
 
+        #
+        # Consult external somthing
+        #
+        try:
+            fp = open("/var/db/plone/" + user_id)
+            user_secret = fp.readline()
+            user_admin  = fp.readline()
+        except:
+            logger.warn("Could not open or read secret file for " + user_id)
+            return None
+
+        user_secret.rstrip("\r\n")
+        user_admin.rstrip("\r\n")
+
+        if secret != user_secret:
+            return None
+        
+        return (user_id, user_id)
+
+    def enumerateUsers(self, id=None, login=None, exact_match=False,
+                       sort_by=None, max_results=None, **kw):
+        key = id or login
+
+        if os.access("/var/db/plone/" + key, os.F_OK):
+            logger.debug("enumerateUsers: " + str(key))
+            return [{'id'       : key,
+                     'login'    : key,
+                     'pluginid' : self.getId()
+                     }]
         return None
     pass
 
-classImplements(EmulabPlugin, IAuthenticationPlugin, IExtractionPlugin)
+classImplements(EmulabPlugin, IAuthenticationPlugin, IUserEnumerationPlugin,
+                IExtractionPlugin)
 InitializeClass(EmulabPlugin)
