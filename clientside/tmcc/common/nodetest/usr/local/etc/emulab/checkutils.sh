238258498b34a6997f00bdaa73f1c48442372aa9
Dan Reading <dreading@flux.utah.edu>
2013-04-24 09:21:48 -0600

    checking for drives /dev/da is a c device not a block device

--- clientside/tmcc/common/nodetest/usr/local/etc/emulab/checkutils.sh
+++ clientside/tmcc/common/nodetest/usr/local/etc/emulab/checkutils.sh
@@ -90,7 +90,7 @@ getdrivenames() {
 	    list="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
 	    for i in $list
 	    do
-		[[ -b /dev/da${i} ]] && drivelist+="/dev/da${i} " 
+		[[ -c /dev/da${i} ]] && drivelist+="/dev/da${i} " 
 		[[ -c /dev/ad${i} ]] && drivelist+="/dev/ad${i} " 
 		[[ -c /dev/amrd${i} ]] && drivelist+="/dev/amrd${i} " 
 	    done
