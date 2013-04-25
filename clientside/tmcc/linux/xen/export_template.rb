8b8a53021b1aa7c299bdeec5406d6131b3185620
Srikanth Raju <srikraju@flux.utah.edu>
2013-04-24 21:25:53 -0600

    Export should push as well

--- clientside/tmcc/linux/xen/export_template.rb
+++ clientside/tmcc/linux/xen/export_template.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/ruby
 
 class EmulabExport
+    
+    attr_accessor :identity
+
+    def initialize()
+        @target = "~~SERVER~~" + ":" + "~~IN_DIR~~" + "~~IN_FILE~~"
+        @user = "~~USER~~"
+        @identity = nil
+    end
 
     def finalize()
         system("rm -Rf ec2-ami-tools-1.4.0.9 > /dev/null 2>&1")
@@ -18,13 +26,13 @@ class EmulabExport
 
         excludes = ['/tmp/image', '/dev', '/media', '/mnt', '/proc', '/sys', '/', '/proc/sys/fs/binfmt_misc', '/dev/pts']
         image = EC2::Platform::Current::Image.new("/",
-						"/tmp/image",
-						10* 1024,
-						excludes,
-						[],
-						true,
-						nil,
-						true)
+                        "/tmp/image",
+                        10* 1024,
+                        excludes,
+                        [],
+                        true,
+                        nil,
+                        true)
         image.make
     end
 
@@ -65,6 +73,20 @@ class EmulabExport
         f.write("ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyHww/vuwQ4adLRzUCKdP5/DubwWJjg/YcGwumuHB24Y0u53KX0qd3oprntw0o/ngntlKXdmAuQ/9lb74Vqpoy0LFVU7adPhNRj1z6WbvRo4cwt5BUBxWlTLQFKs3118kATAkMSKFZbXs54y7GvyFWPTdrgfquEizSaKaPcT3Un0FjWobmK81B7etfSZaaD8fMyWuUHHKYq67ZKDJUc4URRHLMZRIHk7wzbMBV0MEeR7y3se1vBKQDV4IzsTnQF4Mur0HBBc2Kif9oDFh8pykatslvSSjAc8J/t9Lp1RADxon3LHX7TFbvHEgAt0t9g8udOOtw4vB7t9l2VrgV5ZLuw== jetru@myboss.metadata.utahstud.emulab.net\n")
         f.close
     end
+
+    def push_to_emulab()
+        if @identity == nil
+            cmd = "scp emulab.tar.gz " + @user + "@" + @target
+        else
+            cmd = "scp -i " + @identity + " emulab.tar.gz " + @user + "@" + @target
+        end
+        puts "Transferring tarred file with: " + cmd
+        if system(cmd) == false
+            puts "scp failed with error code " + $?.to_s
+            puts "You might want to rerun the push using a SSH keyfile using -i"
+            puts "Also use the -p flag to only attempt the file transfer and no recreate the image"
+        end
+    end
 end
 
 
@@ -72,16 +94,40 @@ if __FILE__ == $0
     raise 'Must run as root' unless Process.uid == 0
 
     ex = EmulabExport.new()
+    pushonly = false
+
+
+    require 'getoptlong'
+    #Parse options
+    opts = GetoptLong.new(
+        ['--identity', '-i', GetoptLong::REQUIRED_ARGUMENT],
+        ['--push-only', '-p', GetoptLong::NO_ARGUMENT]
+    )
+
+    opts.each do |opt, arg|
+        case opt
+            when '--identity'
+                ex.identity = arg
+            when '--push-only'
+                pushonly = true
+        end
+    end
+            
+
     begin
-        ex.check_prereqs
-        ex.create_image
-        ex.get_kernel
-        ex.get_bootopts
-        ex.gen_tar
-        ex.inject_pubkey
+        if pushonly == false
+            ex.check_prereqs
+            ex.create_image
+            ex.get_kernel
+            ex.get_bootopts
+            ex.gen_tar
+        end
+        ex.push_to_emulab
     rescue Exception => e
         print "Error while creating an image: ", e, "\n"
     ensure
         ex.finalize()        
     end
 end
+
+
