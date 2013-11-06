686c632e90074f002322f72981ff68769fbcb5f8
Leigh B Stoller <stoller@flux.utah.edu>
2013-11-06 12:53:39 -0700

    Set a couple of nf_conntrack variables to avoid overflowing NAT tables.

--- clientside/tmcc/linux/xen/libvnode_xen.pm
+++ clientside/tmcc/linux/xen/libvnode_xen.pm
@@ -511,6 +511,18 @@ sub rootPreConfig($)
 	TBScriptUnlock();
 	return -1;
     }
+
+    #
+    # Need these to avoid overflowing the NAT tables.
+    #
+    mysystem("$SYSCTL -w ".
+	     "  net.ipv4.netfilter.ip_conntrack_generic_timeout=120");
+    mysystem("$SYSCTL -w ".
+	     "  net.ipv4.netfilter.ip_conntrack_tcp_timeout_established=54000");
+    mysystem("$SYSCTL -w ".
+	     "  net.netfilter.nf_conntrack_max=131071");
+    mysystem("echo 16384 > /sys/module/nf_conntrack/parameters/hashsize");
+    
     mysystem("touch /var/run/xen.ready");
     TBScriptUnlock();
     return 0;
