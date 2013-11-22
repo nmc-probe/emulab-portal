0fee9f46e97302bea424ce192e3da8c2f0e2c080
Mike Hibler <hibler@cs.utah.edu>
2013-11-22 13:23:50 -0700

    Pass the correct value for RPLR in linkdelays.
    A typo that Dave discovered. He is now the new linktest expert.

--- tmcd/tmcd.c
+++ tmcd/tmcd.c
@@ -3479,7 +3479,7 @@ COMMAND_PROTOTYPE(dolinkdelay)
 			(row[27] ? row[27] : row[0]), row[1],
 			row[2],  row[3],  row[4],  CHECKMASK(row[5]),
 			row[6],	 row[7],  row[8],  row[9],
-			row[10], row[11], row[12], row[14],
+			row[10], row[11], row[12], row[13],
 			row[14], row[15], row[16], row[17], row[18],
 			row[19], row[20], row[21],
 			row[22], row[23], row[24],
