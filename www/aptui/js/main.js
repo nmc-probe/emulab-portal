9fc67760df07c48e89ac26555813c34f7f306ce3
Keith Downie <kdownie@flux.utah.edu>
2014-01-27 12:43:12 -0700

    Fixed a jQuery dependency error with Bootstrap Form Helpers

--- www/aptui/js/main.js
+++ www/aptui/js/main.js
@@ -3,18 +3,19 @@ require.config({
     paths: {
 	'jquery': 'js/lib/jquery-2.0.3.min',
 	'bootstrap': 'bootstrap/js/bootstrap',
+	'formhelpers': 'formhelpers/js/bootstrap-formhelpers',
 	'dateformat': 'js/lib/date.format',
 	'd3': 'js/lib/d3.v3',
     },
     shim: {
 	'bootstrap': { deps: ['jquery'] },
+	'formhelpers': { deps: ['bootstrap']},
 	'dateformat': { exports: 'dateFormat' },
 	'd3': { exports: 'd3' }
     },
 });
 
 require(['jquery', 'js/quickvm_sup',
-	 'formhelpers/js/bootstrap-formhelpers',
 	 // jQuery modules
 	 'bootstrap'],
 function ($, sup)
