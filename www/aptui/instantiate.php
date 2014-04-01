51a53b058dd7743ab00b7c10f4d965e20fe45489
Robert Ricci <ricci@cs.utah.edu>
2014-04-01 01:37:22 -0600

    Change message for email address in use
    This is almost certainly because they have an Emulab account, so tell
    them to log in

--- www/aptui/instantiate.php
+++ www/aptui/instantiate.php
@@ -449,7 +449,7 @@ else {
     $geniuser = GeniUser::LookupByEmail("sa", $formfields["email"]);
     if ($geniuser) {
 	if ($geniuser->name() != $formfields["username"]) {    
-	    $errors["email"] = "Already in use by another user";
+            $errors["email"] = "Already in use - if you have an Emulab account, log in first";
 	    unset($geniuser);
 	}
     }
