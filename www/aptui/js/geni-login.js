938e47343738b7028a0ea3a91f41aa22ae81d6a5
Leigh B Stoller <stoller@flux.utah.edu>
2015-01-26 08:21:20 -0700

    Minor changes to allow geni-login to be used embedded in the Emulab interface.

--- www/aptui/js/geni-login.js
+++ www/aptui/js/geni-login.js
@@ -5,15 +5,18 @@ require(window.APT_OPTIONS.configObject,
 function (_, sup, loginString, waitwaitString)
 {
     'use strict';
+    var embedded = 0;
     
     function initialize()
     {
+	embedded = window.EMBEDDED;
+	
 	$('#page-body').html(loginString);
 	$('#waitwait_div').html(waitwaitString);
 	// We share code with the modal version of login, and the
 	// handler for the button is installed in initialize().
 	// See comment there.
-	sup.InitGeniLogin();
+	sup.InitGeniLogin(embedded);
 	$('#authorize').click(function (event) {
 	    event.preventDefault();
 	    sup.StartGeniLogin();
--- www/aptui/js/login.js
+++ www/aptui/js/login.js
@@ -3,14 +3,17 @@ require(window.APT_OPTIONS.configObject,
 function (_, sup)
 {
     'use strict';
+    var embedded = 0;
     
     function initialize()
     {
+	embedded = window.EMBEDDED;
+
 	// We share code with the modal version of login, and the
 	// handler for the button is installed in initialize().
 	// See comment there.
 	if (window.ISCLOUD) {
-	    sup.InitGeniLogin();
+	    sup.InitGeniLogin(embedded);
 	}
 	window.APT_OPTIONS.initialize(sup);
     }
