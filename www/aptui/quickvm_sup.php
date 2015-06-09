2d7c4247104d62acadf141fae78440fecb296e5f
Leigh B Stoller <stoller@flux.utah.edu>
2015-06-09 16:41:16 -0600

    Add All Profiles for admins.

--- www/aptui/quickvm_sup.php
+++ www/aptui/quickvm_sup.php
@@ -295,7 +295,9 @@ $PAGEHEADER_FUNCTION = function($thinheader = 0, $ignore1 = NULL,
                             History Data</a></li>
 	           <li><a href='sumstats.php'>Summary Stats</a></li>
 	           <li><a href='myexperiments.php?all=1'>
-                            All Experiments</a></li>";
+                            All Experiments</a></li>
+	           <li><a href='myprofiles.php?all=1'>
+                            All Profiles</a></li>";
        }
        else {
            $then = time() - (90 * 3600 * 24);
