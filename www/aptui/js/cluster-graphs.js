b72a16c46bfb79fd88c2e73ac346b25dea8efb05
Leigh B Stoller <stoller@flux.utah.edu>
2015-07-11 17:11:07 -0600

    Bring in Rob's aster graph code. Add 5 minute download of the data from all clusters, and regen the json files. Still need to add live updating.
    NOTE: This is not ready yet, Rob needs to figure out why the labels
    are broken.

---
+++ www/aptui/js/cluster-graphs.js
@@ -0,0 +1,22 @@
+require(window.APT_OPTIONS.configObject,
+	['js/quickvm_sup',
+	 'js/lib/text!template/cluster-graphs.html',
+	 'js/bilevel', 'js/liquidFillGauge'],
+function (sup, clusterString)
+{
+    'use strict';
+
+    function initialize()
+    {
+	window.APT_OPTIONS.initialize(sup);
+
+	$('#cluster-graphs').html(clusterString);
+
+	bilevelAsterGraph("/cloudlab-nofed.json",
+			  "#status-nofed","auto","large");
+	bilevelAsterGraph("/cloudlab-fedonly.json",
+			  "#status-fedonly","auto","large");
+    }
+
+    $(document).ready(initialize);
+});
