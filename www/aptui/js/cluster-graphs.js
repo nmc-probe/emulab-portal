62a6f9c3b0dc1f73af5b0da38001f2cc5ec750ed
Leigh B Stoller <stoller@flux.utah.edu>
2015-07-14 11:41:03 -0600

    Add 30 second refresh.

--- www/aptui/js/cluster-graphs.js
+++ www/aptui/js/cluster-graphs.js
@@ -16,7 +16,26 @@ function (sup, clusterString)
 			  "#status-nofed","auto","large");
 	bilevelAsterGraph("/cloudlab-fedonly.json",
 			  "#status-fedonly","auto","large");
+
+	setTimeout(function f() { Refresh() }, 30000);
     }
 
+    /*
+     * Refresh the graphs.
+     */
+    function Refresh()
+    {
+	$('#status-fedonly').html("");
+	$('#status-nofed').html("");
+	$("div").remove(".d3-tip");
+	
+	bilevelAsterGraph("/cloudlab-nofed.json",
+			  "#status-nofed","auto","large");
+	bilevelAsterGraph("/cloudlab-fedonly.json",
+			  "#status-fedonly","auto","large");
+
+	setTimeout(function f() { Refresh() }, 30000);
+    }
+	
     $(document).ready(initialize);
 });
