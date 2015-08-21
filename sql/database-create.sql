857ca3d444aa6bd76a3d762573064f2f81ee5852
Leigh B Stoller <stoller@flux.utah.edu>
2015-08-21 11:18:19 -0600

    Remove this comment from previous revision, breaks schemadiff:
      -- NOTE: These are mostly pulled from ipmitool.  Other protocols may need
      -- other values or not care at all, hence the addition of OTHER to the 
      -- list and a default of NULL.

--- sql/database-create.sql
+++ sql/database-create.sql
@@ -3527,9 +3527,6 @@ CREATE TABLE `outlets_remoteauth` (
   `key_role` varchar(64) NOT NULL default '',
   `key_uid` varchar(64) NOT NULL default '',
   `mykey` text NOT NULL,
-  -- NOTE: These are mostly pulled from ipmitool.  Other protocols may need
-  -- other values or not care at all, hence the addition of OTHER to the 
-  -- list and a default of NULL.
   `key_privlvl` enum('CALLBACK','USER','OPERATOR','ADMINISTRATOR','OTHER') DEFAULT NULL,
   PRIMARY KEY  (`node_id`,`key_type`,`key_role`,`key_uid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
